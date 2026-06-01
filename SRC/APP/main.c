4a2997c4a664cd38907a8fc3d6b0fa8b1cf98599
CheeseZhang <z_one787@163.com>
2026-06-01 15:27:28 +0800

    🐞 fix: 修复已知问题
    1. 主程序版本v2.0.0-r50_260601
    2. CD版本修复Modbus中老化次数无法保持问题
    3. EF版本点检模式IO和半通道新增中文显示

--- SRC/APP/main.c
+++ SRC/APP/main.c
@@ -189,7 +189,9 @@ void ParameterInit(void)
                                (syspara.GodMode) == GD_AGING     ? "老化模式"
                                : (syspara.GodMode) == GD_FACTORY ? "工厂模式"
                                                                  : "Normal模式");
-                } else if (syspara.GodMode == GD_AGING) {
+                }
+                /* 老化模式下显示老化次数和老化间隔 */
+                if (syspara.GodMode == GD_AGING) {
                         I2CPageRead_Nbytes(ADDR_BURN_CNT, LEN_BURN_CNT, (uint8_t *)&syspara.burnCnt);
                         printd("\r 老化次数: %d", syspara.burnCnt);
                         printd("\r 老化间隔: %d 秒", syspara.agingInterval);
--- SRC/APP/main.h
+++ SRC/APP/main.h
@@ -10,8 +10,8 @@
 // clang-format off
 
 #define DESCRIPTION         "Selector Valve"
-#define SOFTWARE_VERSION    "r49"                /* 软件修改版次 */
-#define SOFT_REVISION       (uint16_t)0x0049     /* 软件修改版次 */
+#define SOFTWARE_VERSION    "r50"                /* 软件修改版次 */
+#define SOFT_REVISION       (uint16_t)0x0050     /* 软件修改版次 */
 
 #define BOARD_0     0x88
 #define BOARD_1     0x66
@@ -113,6 +113,7 @@
 //                              同步QHF Modbus协议,添加参数限幅
 //  v2.0.0CDEF-r49  2026.05.22  修复模式切换无法保持问题,新增70号寄存器老化间隔
 //                              新增协议切换限制
+//  v2.0.0CDEF-r50  2026.06.01  修复Modbus中老化次数无法保持问题,点检模式IO和半通道新增中文显示
 
 
 
